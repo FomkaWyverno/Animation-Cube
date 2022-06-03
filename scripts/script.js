755b9cf9508b06f66adc316db9a9486f4ab7ea5d
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-06-03 12:39:59 +0300

    Click event

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,29 @@ const left_bottom = document.querySelector(".cube__left-bottom");
 const right_bottom = document.querySelector(".cube__right-bottom");
 
 setTimeout(() => {
-    left_top.classList.add("left-top-passive");
-    right_top.classList.add("right-top-passive");
-    left_bottom.classList.add("left-bottom-passive");
-    right_bottom.classList.add("right-bottom-passive");
+    togglePulse();
     setTimeout(() => {
-        cube_element.classList.add("cube-rotate");
+        toggleRotate();
     },100);
     cube_element.addEventListener('click',event => {
-        console.log('click');
+        togglePulse();
+        togglePausedRotate();
+        for (let i = 0; i < cube_element.children.length; i++) {
+            cube_element.children[i].classList.toggle('cube-start-reverse');
+        }
     });
 },2900);
+
+function togglePulse() {
+    left_top.classList.toggle("left-top-passive");
+    right_top.classList.toggle("right-top-passive");
+    left_bottom.classList.toggle("left-bottom-passive");
+    right_bottom.classList.toggle("right-bottom-passive");
+}
+
+function toggleRotate() {
+    cube_element.classList.toggle("cube-rotate");
+}
+function togglePausedRotate() {
+    cube_element.classList.toggle("cube-rotate-paused");
+}
